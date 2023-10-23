0b0669dae911b52ab2567b9020c572aefcba0f71
Aserrano7 <serranoandres022@gmail.com>
2023-10-23 11:32:44 -0700

    added READ.ME and Comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,16 @@
-// Assignment code here
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+  // create arrays that will hold all the values of each type
   var charLowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var charUppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   var charNum = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   var charSpecial = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
 
-  var choiceLength = window.prompt("Enter how long you want your password to be from 8-128 characters");
+  // ask the user how long you want  the array to be and checks if you inputed the right type
+  var choiceLength = prompt("Enter how long you want your password to be from 8-128 characters");
   if (!choiceLength || choiceLength < 8 || choiceLength > 128) {
     alert("Please choose a number between 8-128 characters long.");
     return;
@@ -17,14 +18,17 @@ function generatePassword() {
     alert("Needs to be a number.");
     return;
   }
+  // create the propmt so you can save their choice
   var choiceLowercase = confirm("Press OK or Cancel if you want your password to have lowercase.");
   var choiceUppercase = confirm("Press OK or Cancel if you want your password to have Uppercase.");
   var choiceNum = confirm("Press OK or Cancel if you want your password to have Numbers.");
-  var choiceSpecial = confirm("Press OK or Cancel if you want you password to have Specail Characters.");
+  var choiceSpecial = confirm("Press OK or Cancel if you want you password to have Special Characters.");
+  // checks if at least one has been selected
   if (choiceLowercase === false && choiceUppercase === false && choiceNum === false && choiceSpecial === false) {
-    var needToChooseOne = window.alert('You must choose atleast one');
+    var needToChooseOne = alert('You must choose atleast one');
     return;
   }
+  //combines all arrays into one big array if the choice is true 
   var combineArrays = [];
   if (choiceLowercase === true) {
     combineArrays = [].concat(charLowercase);
@@ -40,9 +44,8 @@ function generatePassword() {
       combineArrays = combineArrays.concat(charSpecial);
 
     }
-
-   
-
+    // created an array to save all the random values we get from the combine arrays and we push them 
+    //through to be save on another array called password
     var password = [];
     for (let i = 0; i < choiceLength; i++) {
       var index = Math.floor(Math.random() * combineArrays.length);
@@ -51,59 +54,7 @@ function generatePassword() {
     }
     return password.join("");
   }
-  /*  var tempCharLowercase = charLowercase;
-   var tempCharUppercase = charUppercase;
-   var tempCharNum = charNum;
-   var tempCharSpecial = charSpecial;
-
-   if (choiceLowercase === false) {
-     charLowercase = [];
-   }
-   if (choiceUppercase === false) {
-     charUppercase = [];
-   }
-   if (choiceNum === false) {
-     charNum = [];
-   }
-   if (choiceSpecial === false) {
-     charSpecial = [];
-   }
-   var allArrays = [];
-   let i = 0;
-   do {
-     if (charLowercase.length > 0) {
-       var index = Math.floor(Math.random() * tempCharLowercase.length);
-       var computerLowercase = tempCharLowercase[index];
-       allArrays.push(computerLowercase);
-       i++;
-     }
-
-     if (charUppercase.length > 0) {
-       var index2 = Math.floor(Math.random() * tempCharUppercase.length);
-       var computerUppercase = tempCharUppercase[index2];
-       allArrays.push(computerUppercase);
-       i++;
-     }
-     if (charNum.length > 0) {
-       var index3 = Math.floor(Math.random() * tempCharNum.length);
-       var computerNum = tempCharNum[index3];
-       allArrays.push(computerNum);
-       i++;
-     }
-     if (charSpecial.length > 0) {
-       var index4 = Math.floor(Math.random() * tempCharSpecial.length);
-       var computerSpecial = tempCharSpecial[index4];
-       allArrays.push(computerSpecial);
-       i++;
-     }
-   }
-   while (i < choiceLength);
-   let result = allArrays.join("");
-   console.log(result);
-   return result;
- } */
   // Write password to the #password input
-
   function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Password-Generator
+
+## Description
+This weeks challenge was all about javascript. We had to create a password generator, that would take a users input to match the correct selected choices. Whether that was having lowercase, uppercase, numbers,and special characters. I learned that javascript can be a very powerful tool when it comes to user interaction with the webpage, and how to use the users input to create a fuctioning webpage. I ran into a problem where my password was randonmly generated, but it was in the same order as the if statements were testing if that choice was selcected and pushing that into a bigger array. Luckly during my tutoring session, Ramon pointed that out to me and I was able to overcome this edge case. Now this password generator works properly and can generate a password, anywhere from 8 characters long to 128 characters long, while also checking if the user wants lowercase, uppercase, numbers, or special characters in their password.
+## Usage
+This image demostrates the web application appearance and fuctionality
+![This is a screenshot of the whole webpage](Develop/images/1.JPG).
+![This is a screenshot of the whole webpage](Develop/images/2.JPG).
+![This is a screenshot of the whole webpage](Develop/images/3.JPG).
+![This is a screenshot of the whole webpage](Develop/images/4.JPG).
+![This is a screenshot of the whole webpage](Develop/images/5.JPG).
+![This is a screenshot of the whole webpage](Develop/images/6.JPG).
+![This is a screenshot of the whole webpage](Develop/images/7.JPG).
+
+## Credits
+I used the activites in the UCSD folder to help me with this project as well as the help of a tutor, that helped me understand my code wasn't what they were looking for.
+## License
+N/A
+
+## Features
+Can generate a random password that has the selected choices of the the users input.
+
+## Deployed Application
+https://aserrano7.github.io/Password-Generator/
